3811463a089013bbceb3a164bc9ca5ec5235b87f
Manzurur Khan <iamsidky@gmail.com>
2020-02-02 15:59:22 -0500

    Firebase config

--- storage/firebase.go
+++ storage/firebase.go
@@ -4,12 +4,18 @@ import (
 	"cloud.google.com/go/firestore"
 	"context"
 	firebase "firebase.google.com/go"
+	"google.golang.org/api/option"
+	"os"
 )
 
 func InitStorage() (*firestore.Client, error) {
+
+	credentialBytes := os.Getenv("FIREBASE_CRED")
+
+	opt := option.WithCredentialsJSON([]byte(credentialBytes))
 	ctx := context.Background()
-	conf := &firebase.Config{ProjectID:"comical-266916"}
-	app, err := firebase.NewApp(ctx, conf)
+	//conf := &firebase.Config{ProjectID:"comical-266916"}
+	app, err := firebase.NewApp(ctx, nil, opt)
 	if err != nil {
 		return nil, err
 	}

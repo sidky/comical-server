d4cf42b48e86aaebc42353d96b4d022e55b3076c
Manzurur Khan <iamsidky@gmail.com>
2020-03-01 14:02:27 -0500

    Change path to md5

--- storage/comics_store.go
+++ storage/comics_store.go
@@ -4,6 +4,8 @@ import (
 	"comical/model/comics"
 	"comical/model/store"
 	"context"
+	"crypto/md5"
+	"encoding/hex"
 	"fmt"
 	"log"
 )
@@ -12,11 +14,17 @@ func NewComicsEntry(ctx context.Context, item *comics.Entry) error {
 
 	entryName := fmt.Sprintf("comics.%s", item.Origin)
 
-	r, err := GetClient().Collection("comics").Doc(item.Origin).Collection("entries").Doc(item.ID).Set(ctx, store.ToStoreEntry(item))
+	r, err := GetClient().Collection("comics").Doc(item.Origin).Collection("entries").Doc(generatePath(item.ID)).Set(ctx, store.ToStoreEntry(item))
 	log.Printf("Result: %v", r)
 	if err != nil {
 		return err
 	}
 	log.Printf("Successfully updated %s", entryName)
 	return nil
+}
+
+func generatePath(id string) string {
+	hasher := md5.New()
+	hasher.Write([]byte(id))
+	return hex.EncodeToString(hasher.Sum(nil))
 }
